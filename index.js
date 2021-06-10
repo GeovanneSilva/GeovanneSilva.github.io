2e7028905950088547741b375b7c8a221eeaf12d
Geovanne Silva <66743857+GeovanneSilva@users.noreply.github.com>
2021-06-10 15:25:53 -0300

    Atualizando

---
+++ index.js
@@ -0,0 +1,11 @@
+﻿Email.send({
+    Host : "smtp.yourisp.com",
+    Username : "username",
+    Password : "password",
+    To : 'them@website.com',
+    From : "you@isp.com",
+    Subject : "This is the subject",
+    Body : "And this is the body"
+}).then(
+  message => alert(message)
+);
